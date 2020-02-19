4f015399a3b12a3d02c712b65eb52df555b05478
kimhg9511 <54794439+kimhg9511@users.noreply.github.com>
2020-02-19 10:36:35 +0900

    20200218
    source code update
    toggle key
    (ctrl + space, ctrl + alt + down, ctrl + / etc..)
    System.out

---
+++ javabasic/src/javabasic/Hello.java
@@ -0,0 +1,19 @@
+package javabasic;
+
+public class Hello {
+	public static void main(String[] args) {
+		int age = 0;
+		
+		System.out.println(age);
+		System.out.println("조재석님,이수연님");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+		System.out.println("6");
+		System.out.println("7");
+		System.out.println("8");
+		System.out.println("9");
+		System.out.println("11");
+	}
+	
+}
