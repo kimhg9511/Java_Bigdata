bf7d1fcc417ff2e1787d8b0e01492f7537077425
kimhg9511 <54794439+kimhg9511@users.noreply.github.com>
2020-02-20 15:53:32 +0900

    20200220_master
    commit oriented eclipse codes at gitdesktop.

---
+++ src/OperationTest09.java
@@ -0,0 +1,17 @@
+
+public class OperationTest09 {
+	public static void main(String[] args) {
+		int hex = 0xABCD;
+		int mask = 0xF;
+		
+		System.out.printf("hex=%x%n",hex);
+		System.out.printf("%X%n", hex & mask);
+		
+		hex = hex >> 4;
+		System.out.printf("%X%n", hex & mask);
+		hex = hex >> 4;
+		System.out.printf("%X%n", hex & mask);
+		hex = hex >> 4;
+		System.out.printf("%X%n", hex & mask);
+	}
+}
